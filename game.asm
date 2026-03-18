f3a64be2e7abc47774658f02a9e1bd3d7bf9488c
Jimmy Feng Feng <jimmy.feng04@gmail.com>
2026-03-18 15:53:45 -0600

    Tecla de inicio

--- game.asm
+++ game.asm
@@ -1,6 +1,25 @@
 bits 16
 org 0x1000
 
+start:
+    mov si, prompt         ; mensaje de bienvenida
+
+print_string:              ; print del mensaje de bienvenida
+    lodsb
+    cmp al, 0              ; si ya termino
+    je wait_enter          ; esperar tecla enter
+
+    mov ah, 0x0E           
+    int 0x10               ; interrupcion de video
+    jmp print_string       
+
+wait_enter:
+    mov ah, 00h
+    int 16h                 ; espera una tecla
+
+    cmp al, 0Dh             ; tecla enter
+    jne wait_enter
+
 random_pos:
     mov ah, 00h             ;interrupcion para tomar el system time
     int 1AH                 ;[CX:DX] en ese rango esta la cantidad del ticks desde media noche
@@ -9,14 +28,15 @@ random_pos:
     mov cx, 21              ;divisor
     div cx                  ;el residuo entre 0-20 queda en dx
     add dx, 10              ;ahora queda entre 10-30
+    
+    mov dl, dl              ;columna
+    mov dh, dl              ;row
+    mov si, message         ;un pointer al mensaje
 
-mov dl, dl                  ;columna
-mov dh, dl                  ;row
-mov si, message             ;un pointer al mensaje
-
-;guardar pos inicial
-mov [start_col], dl
-mov [start_row], dh
+    ; guardar posicion inicial
+    mov [start_col], dl
+    mov [start_row], dh
+    jmp set_cursor
 
 set_cursor:
     mov bh, 0               ;display page
@@ -40,19 +60,23 @@ wait_key:
     int 16h                 ;interrupcion de tecla
 
     cmp ah, 4Bh             ;flecha izquierda
-    je print
+    je print                
+
     cmp ah, 4Dh             ;flecha derecha
     je print
+
     cmp ah, 48h             ;flecha arriba
     je print
+    
     cmp ah, 50h             ;flecha abajo
+    je print
 
     jmp wait_key
-
 
 done:
     jmp $
 
+prompt    db "dar enter para empezar", 0
 message db "onichan",0          ;variable guardada en memoria
 start_col db 0
 start_row db 0
