6625c2a93a5774f544c1e885fc749e558f1adaa0
Jimmy Feng Feng <jimmy.feng04@gmail.com>
2026-03-18 20:07:20 -0600

    print derecha

--- game.asm
+++ game.asm
@@ -23,55 +23,30 @@ wait_enter:
     call clear_screen       ; limpiar ventana
 
 random_pos:
-
-    ; random para columna: 6..73
-    mov ah, 00h             ;interrupcion para tomar el system time
-    int 1AH                 ;[CX:DX] en ese rango esta la cantidad del ticks desde media noche
-    
-    mov ax, dx              ;dx como semilla
-    xor dx, dx
-    mov cx, 68              ;0..67 
-    div cx                  ;residuo
-
-    xchg ax, dx             ;mover residuo a ax
-    mov dl, al              ;usar al como columna base
-    add dl, 6               ;ajustar rango 6..73
-
-    mov [start_col], dl     ;guardar col inicial
-
-    ; random para fila: 6..18
-    mov ah, 00h          
-    int 1AH
-
+    ;random columna 
+    mov ah, 00h             ; interrupcion para tomar el system time
+    int 1Ah                 ; [CX:DX] = ticks desde media noche
     mov ax, dx
     xor dx, dx
-    mov cx, 13              ;0..12
-    div cx                  ;residuo
+    mov cx, 68              ; divisor
+    div cx                  ; residuo entre 0..67 queda en DX
+    add dl, 6               ; ahora queda entre 6..73
+    mov [start_col], dl     ; guardar columna inicial
+    ;random fila
+    mov ah, 00h             ; interrupcion para tomar el system time
+    int 1Ah
+    mov ax, dx
+    xor dx, dx
+    mov cx, 13              ; divisor
+    div cx                  ; residuo entre 0..12 queda en DX
+    add dl, 6               ; ahora queda entre 6..18
+    ;guardar pos inicial
+    mov dh, dl              ; fila
+    mov dl, [start_col]     ; columna
+    mov [start_row], dh     ; guardar fila inicial
 
-    xchg ax, dx             
-    mov dh, al              ;usar al como fila base
-    add dh, 6               ;ajustar rango 6..18
-
-    mov [start_row], dh     ;guardar row inicial
-
-    mov si, message         ;un pointer al mensaje
-    jmp set_cursor
-
-set_cursor:
-    mov bh, 0               ;display page
-    mov ah, 02h             ;poner el cursor en la posicion
-    int 0x10                ;interrupcion de video
-
-print_horizontal:
-    mov ah, 0x0E            ;la verdad nose, igual no es importante uwu
-    lodsb                   ;agarra un byte de message lo carga y suma 1
-    cmp al, 0               ;se terminaron las letras?
-    je wait_key             ;vaya a done si se terminaron las letras
-    int 0x10                ;interrupcion de video
-    ;inc dh                 ;incrementa el row (comentado por ahora para q sea horizontal)
-    inc dl                  ;incrementa el column
-    jmp set_cursor          ;set el curson con la nueva posicion
-
+    mov si, message         ; puntero al mensaje
+    jmp print_horizontal
 
 ; esperar tecla
 wait_key:
@@ -82,29 +57,51 @@ wait_key:
     je done
 
     cmp ah, 4Bh             ;flecha izquierda: rota 90 grados hacia izquierda sobre eje y
-    ;je print_right               
+    je print_left               
 
     cmp ah, 4Dh             ;flecha derecha: rota 90 grados hacia derecha sobre eje y
     je print_right
 
     cmp ah, 48h             ;flecha arriba: rota 180 grados hacia abajo sobre eje x
-    ;je print
+    je print_horizontal
     
     cmp ah, 50h             ;flecha abajo: rota 180 grados hacia arriba sobre eje c
     ;je print
 
     jmp wait_key
 
-print_right:
-    call clear_screen       ;limpiar todo
-    mov si, message         ;puntero al mensaje
-    mov dl, [start_col]     ;cargar col inicial
-    mov dh, [start_row]     ;cargar row inicial
-
-pr_loop:
+set_cursor:
     mov bh, 0               ;display page
-    mov ah, 02h             ;cursor en pos
-    int 0x10                ;int video
+    mov ah, 02h             ;poner el cursor en la posicion
+    int 0x10                ;interrupcion de video
+    ret
+
+start_print:   
+    call clear_screen
+    mov si, message
+    mov dl, [start_col]
+    mov dh, [start_row]  
+    ret     
+
+print_horizontal:
+    call start_print
+ph_loop:
+    call set_cursor
+    mov ah, 0x0E            ;la verdad nose, igual no es importante uwu
+    lodsb                   ;agarra un byte de message lo carga y suma 1
+    cmp al, 0               ;se terminaron las letras?
+    je wait_key             ;vaya a done si se terminaron las letras
+    int 0x10                ;interrupcion de video
+    ;inc dh                 ;incrementa el row (comentado por ahora para q sea horizontal)
+    inc dl                  ;incrementa el column
+    jmp ph_loop             ;set el curson con la nueva posicion
+
+
+
+print_right:
+    call start_print
+pr_loop:
+    call set_cursor
 
     lodsb                   ;cargar el siguiente byte del mensaje
     cmp al, 0               ;si se termino el mensaje
@@ -114,7 +111,22 @@ pr_loop:
     int 0x10                ;int video
 
     inc dh                  ;incrementa el row para imprimir hacia abajo
-    jmp pr_loop            
+    jmp pr_loop  
+
+print_left:
+    call start_print
+pl_loop:
+    call set_cursor
+
+    lodsb
+    cmp al, 0
+    je wait_key
+
+    mov ah, 0Eh
+    int 10h
+
+    dec dh                  ; sube desde la 'o'
+    jmp pl_loop
 
 done:
     jmp $
@@ -126,11 +138,11 @@ clear_screen:
     mov cx, 0000h           ;esquina superior izquierda
     mov dx, 184Fh           ;esquina inferior derecha (24 filas, 80 columnas)
     int 10h                 ;int video
-    ret
+    ret  
 
 ;variables guardadas en memoria
-prompt    db "dar enter para empezar", 0
-message db "onichan",0          
+prompt db "dar enter para empezar", 0
+message db "onichan",0      
 start_col db 0
 start_row db 0
 step_col  db 0
