cae1e75f26ea32f2bef3348bd66be6c5c2adab33
joscCE <j.campos@estudiantec.cr>
2026-03-19 20:41:14 -0600

    por_si_acaso

--- boot.asm
+++ boot.asm
@@ -1,19 +1,35 @@
 bits 16
 org 0x7C00
 
+
+cli
+xor ax, ax
+mov ds, ax
+mov es, ax
+mov ss, ax
+mov sp, 0x7C00
+sti
+
 mov [BOOT_DRIVE], dl ; BIOS pone el disco en DL
+
+
 
 mov bx, 0x1000       ; donde cargar el programa
 mov dh, 1            ; cuantos sectores leer
 
+mov ah, 0x0E     
+mov al, 'X'      
+int 0x10  
+
+
 call disk_load
 
-jmp 0x1000           ; saltar al game
+jmp 0x000:0x1000           ; saltar al game
 
 ;-----------------------
 disk_load:
     mov ah, 0x02     ; funcion leer sectores
-    mov al, dh       ; cantidad de sectores
+    mov al, 1       ; cantidad de sectores
     mov ch, 0x00     ; cilindro
     mov dh, 0x00     ; cabeza
     mov cl, 0x02     ; sector (1 es bootloader)
@@ -24,6 +40,13 @@ disk_load:
     ret
 
 disk_error:
+    mov ah, 0x0E
+    mov al, 'E'
+    int 0x10
+
+    mov al, ah
+    int 0x10
+
     jmp $
 
 BOOT_DRIVE db 0
